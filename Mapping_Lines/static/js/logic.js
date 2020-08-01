86487370aba84ba508078e9f1f7d25343762536c
lkaruppiah54462 <lakshmazda@gmail.com>
2020-07-31 20:08:18 -0700

    logic change for line

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -8,26 +8,48 @@ console.log("working");
 let map = L.map("mapid", {
     center: [
 	  //40.7, -94.5
-	  34.0522, -118.2437
+	  37.6213, -88.0
 	],
     zoom: 4
   });
 
+// Coordinates for each point to be used in the line.
+let line = [
+	[37.6213, -122.3790],
+	[30.1975,-97.6664],
+	[43.6667, -79.6248],
+	[40.6413, -73.7781]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+	color: "blue",
+	weight : 4,
+	dashArray : '20 20',
+	dashOffset : 0,
+	opacity:0.5
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-// 	maxZoom: 18,
-// 	accessToken: API_KEY
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// 	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+// 		maxZoom: 18,
+// 		accessToken: API_KEY
 // });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-		maxZoom: 18,
-		accessToken: API_KEY
-});
-
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// 	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+// 		maxZoom: 18,
+// 		accessToken: API_KEY
+// });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
@@ -53,3 +75,4 @@ cityData.forEach(function(city) {
 // 	color : 'black',
 // 	fillColor: '#FFFF66'
 //  }).addTo(map);
+
